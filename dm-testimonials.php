e5c7de3737f686d63a302c2e25b6e7fe778cd226
Bradford Knowlton <brad@ucanbonline.net>
2016-01-19 19:39:29 -0700

    fixed capitalization issue

--- dm-testimonials.php
+++ dm-testimonials.php
@@ -9,32 +9,32 @@ Version: 1.6
 Author URI: http://bradknowlton.com/
 */
 
-add_action( 'init', 'register_cpt_testimonial' );
-function register_cpt_testimonial() {
+add_action( 'init', 'register_cpt_Testimonial' );
+function register_cpt_Testimonial() {
 	
 	$labels = array(
-	'name' => _x( 'TestImonial Categories', 'testimonial_categories' ),
-	'singular_name' => _x( 'TestImonial Category', 'testimonial_categories' ),
-	'search_items' => _x( 'Search TestImonial Categories', 'testimonial_categories' ),
-	'popular_items' => _x( 'Popular TestImonial Categories', 'testimonial_categories' ),
-	'all_items' => _x( 'All TestImonial Categories', 'testimonial_categories' ),
-	'parent_item' => _x( 'Parent TestImonial Category', 'testimonial_categories' ),
-	'parent_item_colon' => _x( 'Parent TestImonial Category:', 'testimonial_categories' ),
-	'edit_item' => _x( 'Edit TestImonial Category', 'testimonial_categories' ),
-	'update_item' => _x( 'Update TestImonial Category', 'testimonial_categories' ),
-	'add_new_item' => _x( 'Add New TestImonial Category', 'testimonial_categories' ),
-	'new_item_name' => _x( 'New TestImonial Category', 'testimonial_categories' ),
-	'separate_items_with_commas' => _x( 'Separate testimonial categories with commas', 'testimonial_categories' ),
-	'add_or_remove_items' => _x( 'Add or remove testimonial categories', 'testimonial_categories' ),
-	'choose_from_most_used' => _x( 'Choose from the most used testimonial categories', 'testimonial_categories' ),
-	'menu_name' => _x( 'TestImonial Categories', 'testimonial_categories' ),
+	'name' => _x( 'Testimonial Categories', 'Testimonial_categories' ),
+	'singular_name' => _x( 'Testimonial Category', 'Testimonial_categories' ),
+	'search_items' => _x( 'Search Testimonial Categories', 'Testimonial_categories' ),
+	'popular_items' => _x( 'Popular Testimonial Categories', 'Testimonial_categories' ),
+	'all_items' => _x( 'All Testimonial Categories', 'Testimonial_categories' ),
+	'parent_item' => _x( 'Parent Testimonial Category', 'Testimonial_categories' ),
+	'parent_item_colon' => _x( 'Parent Testimonial Category:', 'Testimonial_categories' ),
+	'edit_item' => _x( 'Edit Testimonial Category', 'Testimonial_categories' ),
+	'update_item' => _x( 'Update Testimonial Category', 'Testimonial_categories' ),
+	'add_new_item' => _x( 'Add New Testimonial Category', 'Testimonial_categories' ),
+	'new_item_name' => _x( 'New Testimonial Category', 'Testimonial_categories' ),
+	'separate_items_with_commas' => _x( 'Separate Testimonial categories with commas', 'Testimonial_categories' ),
+	'add_or_remove_items' => _x( 'Add or remove Testimonial categories', 'Testimonial_categories' ),
+	'choose_from_most_used' => _x( 'Choose from the most used Testimonial categories', 'Testimonial_categories' ),
+	'menu_name' => _x( 'Testimonial Categories', 'Testimonial_categories' ),
 	);
 
     $args = array(
     'labels' => $labels,
     'hierarchical' => false,
     'supports' => array( 'title', 'editor', 'excerpt', 'author',  ), // 'thumbnail', 'custom-fields', 'revisions'
-    // 'taxonomies' => array( 'testimonial_categories' ),
+    // 'taxonomies' => array( 'Testimonial_categories' ),
     'public' => true,
     'show_ui' => true,
     'show_in_menu' => true,
@@ -47,7 +47,7 @@ function register_cpt_testimonial() {
     'rewrite' => false,
     'capability_type' => 'post'
     );
-    register_post_type( 'testimonial', $args );
+    register_post_type( 'Testimonial', $args );
 	
 		
 }
